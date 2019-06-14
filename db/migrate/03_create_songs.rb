3029ad58e1c248ec2a644a9a2cadcffa06470b31
Alexander Riccio <test35965+learn@gmail.com>
2019-06-14 16:03:05 -0400

    solve lab

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -2,8 +2,8 @@ class CreateSongs < ActiveRecord::Migration[4.2]
     def change
         create_table(:songs) do |t|
             t.string :name
-            t.integer :artist_id
-            t.integer :genre_id
+            t.integer :artist_id, foreign_key: true
+            t.integer :genre_id, foreign_key: true
         end
     end
 end
